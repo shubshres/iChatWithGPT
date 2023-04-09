fcb3a0eabf53a5500507337e38211b587faac0b9
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2023-04-09 14:19:38 -0500

    Initial Commit

---
+++ ChatWithGPT/ChatWithGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithGPTApp.swift
+//  ChatWithGPT
+//
+//  Created by Shubhayu Shrestha on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatWithGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
