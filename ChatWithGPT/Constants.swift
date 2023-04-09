afc2fd92fb73246275b03622b709fefe331799c9
Shubhayu Shrestha <shresthashubhayu@gmail.com>
2023-04-09 17:18:05 -0500

    initial build

---
+++ ChatWithGPT/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  ChatWithGPT
+//
+//  Created by Shubhayu Shrestha on 4/9/23.
+//
+
+import Foundation
---
+++ ChatWithGPT/OpenAIService.swift
@@ -0,0 +1,8 @@
+//
+//  OpenAIService.swift
+//  ChatWithGPT
+//
+//  Created by Shubhayu Shrestha on 4/9/23.
+//
+
+import Foundation
